5f763395afcb15c4aa49b9d1b550c94b18653519
nobisewashi930 <37402565+nobisewashi930@users.noreply.github.com>
2020-09-20 18:43:47 +0700

    Add files via upload

---
+++ geometric.py
@@ -0,0 +1,43 @@
+ 
+from math import log2 as log
+
+
+def prob(n, p):
+	return (1 - p) ** (n - 1) * p
+
+
+def infoMeasure(n, p):
+	return -log(prob(n, p))
+
+
+def sumProb(N, p):
+	'''
+	Parameters:
+			N (int): number of trials
+			p (float): success probability
+	Returns:
+			cdf (float): CDF of Geometric distribution w.r.t N
+	'''
+
+	return 1 - (1 - p) ** N
+
+
+def approxEntropy(N, p):
+	'''
+	For example with p = 0.5
+			Ent = 1.9 when N = 7
+			Ent = 1.82 when N = 10
+		=> Ent converge to 2
+		Parameters:
+			N (int): number of trials
+			p (float): success probability
+		Returns:
+			Ent (float): accumulate Entropy to N trials.
+	'''
+	
+	Ent = 0
+	for k in range(1, N + 1):
+		Pr = prob(k, p)
+		I = infoMeasure(k, p)
+		Ent += Pr * I
+	 return Ent 
